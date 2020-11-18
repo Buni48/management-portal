ef4a4aebefe8b5d3e672e26f6b0d0cc1be96ec89
Buni48 <dago24@posteo.de>
2020-11-18 18:23:41 +0100

    Updated database structure

--- customers/models.py
+++ customers/models.py
@@ -2,9 +2,45 @@ from django.db import models
 
 class Customer(models.Model):
     customer_number = models.CharField(max_length = 32)
-    company         = models.CharField(max_length = 64)
+    name            = models.CharField(max_length = 64)
+
+class Location(models.Model):
+    name            = models.CharField(max_length = 64)
+    email_address   = models.CharField(max_length = 64)
+    phone_number    = models.CharField(max_length = 64)
+    street          = models.CharField(max_length = 64)
+    house_number    = models.CharField(max_length = 8)
+    postcode        = models.CharField(max_length = 16)
+    city            = models.CharField(max_length = 64)
+    adviser         = models.ForeignKey(
+        to                  = 'CustomerAdviser',
+        on_delete           = models.SET_NULL,
+        related_name        = 'advisers',
+        related_query_name  = 'adviser',
+        null                = True,
+    )
+
+class Person(models.Model):
     first_name      = models.CharField(max_length = 64)
     last_name       = models.CharField(max_length = 64)
     email_address   = models.CharField(max_length = 64)
     phone_number    = models.CharField(max_length = 64)
-    active          = models.BooleanField(default = True)
+    class Meta:
+        abstract = True
+
+class ContactPerson(Person):
+    product  = models.ManyToManyField(
+        to                  = 'licences.SoftwareProduct',
+        related_name        = 'contact_persons',
+        related_query_name  = 'contact_person',
+    )
+    location = models.ForeignKey(
+        to                  = 'Location',
+        on_delete           = models.CASCADE,
+        related_name        = 'contact_persons',
+        related_query_name  = 'contact_person',
+        null                = True,
+    )
+
+class CustomerAdviser(Person):
+    pass
--- updates/models.py
+++ updates/models.py
@@ -3,5 +3,11 @@ from django.db import models
 class Update(models.Model):
     version      = models.CharField(max_length = 16)
     release_date = models.DateTimeField(auto_now_add = True)
-    software     = models.ForeignKey(to = 'licences.Software', on_delete = models.CASCADE, null = False)
-    active       = models.BooleanField(default = True)
+    content      = models.BinaryField()
+    product      = models.ForeignKey(
+        to                  = 'licences.SoftwareProduct',
+        on_delete           = models.CASCADE,
+        related_name        = 'updates',
+        related_query_name  = 'update',
+        null                = False,
+    )
