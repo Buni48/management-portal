8ace723586b818481891298283bf51c9cfd1be1c
Ruvii <1825861@stud.hs-mannheim.de>
2020-11-20 13:58:01 +0100

    deleted app_name

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'customers'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.customerList, name='customers_list'),
