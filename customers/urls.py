da73390f250beb6aadfabbc31586cd03589d6367
Ruvii <1825861@stud.hs-mannheim.de>
2020-11-20 13:24:02 +0100

    added def customer to views.py and path to urls.py

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'customers'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.customerList, name='customers_list'),
+    path('customer/', views.customer, name='customer'),
 ]
