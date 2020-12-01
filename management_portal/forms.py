290eec123eaf17344fdf35d5b8a738d9f693f07f
Buni48 <dago24@posteo.de>
2020-12-01 14:50:32 +0100

    Global search: auto table create + delete

--- management_portal/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    search_word = forms.CharField(
-        label       = 'Globale Suche',
-        min_length  = 3,
-        max_length  = 64,
-    )
