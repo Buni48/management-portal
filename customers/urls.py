c304f4dcfaed7f3c5e9e50ea1516ea6bc25d3a0d
SimonLichterfeld <Bayern21_>
2020-12-13 13:14:18 +0100

    Added feature to save Customers to database via form

--- customers/urls.py
+++ customers/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.customerList, name='customers_list'),
     path('customer/<int:id>/', views.customer, name='customer'),
+    path('create/', views.addCustomer, name='createCustomer'),
+    path('save/', views.saveCustomer, name='saveCustomer'),
 ]
