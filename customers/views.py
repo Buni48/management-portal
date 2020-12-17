e2a0c3e3ee9963b320ff8e3a74cbcc5c77caee68
Buni48 <dago24@posteo.de>
2020-12-17 19:21:09 +0100

    Location create, edit & delete: views

--- customers/views.py
+++ customers/views.py
@@ -55,13 +55,15 @@ def customer(request: WSGIRequest, id: int = 0):
     }
     if not response:
         response = render(request, 'customers/customer.html', context = context)
+        response.delete_cookie('customer_status_status')
+        response.delete_cookie('customer_status_message')
     return response
 
 def create(request: WSGIRequest) -> HttpResponse:
     heartbeats = HeartbeatController.read()
     context = {
         'title'     : 'Kunden erstellen',
-        'heartbeats': heartbeats
+        'heartbeats': heartbeats,
     }
     return render(request, 'customers/edit.html', context)
 
@@ -100,6 +102,75 @@ def delete(request: WSGIRequest, id: int = 0) -> HttpResponse:
     response = redirect('customers_list')
     if id > 0:
         status = CustomerController.delete(id = id)
+        response.set_cookie('customer_status_status' , status.status , 7)
+        response.set_cookie('customer_status_message', status.message, 7)
+
+    return response
+
+def createLocation(request: WSGIRequest, customer_id: int = 0) -> HttpResponse:
+    if customer_id < 1:
+        return redirect('customers_list')
+
+    heartbeats  = HeartbeatController.read()
+    customer    = CustomerController.getCustomerById(id = customer_id)
+    context     = {
+        'title'     : 'Standort erstellen',
+        'customer'  : customer,
+        'heartbeats': heartbeats,
+    }
+    return render(request, 'customers/edit-location.html', context)
+
+def editLocation(request: WSGIRequest, id: int = 0, customer_id: int = 0) -> HttpResponse:
+    if id < 1 or customer_id < 1:
+        return redirect('customers_list')
+
+    location   = LocationController.getLocationById(id = id)
+    heartbeats = HeartbeatController.read()
+    customer   = CustomerController.getCustomerById(id = customer_id)
+    context    = {
+        'title'     : 'Standort bearbeiten',
+        'location'  : location,
+        'customer'  : customer,
+        'heartbeats': heartbeats,
+    }
+    return render(request, 'customers/edit-location.html', context)
+
+def saveLocation(request: WSGIRequest) -> JsonResponse:
+    response = JsonResponse({})
+    if request.is_ajax:
+        id            = request.POST.get('id', '')
+        name          = request.POST.get('name', '')
+        email_address = request.POST.get('email_address', '')
+        phone_number  = request.POST.get('phone_number', '')
+        street        = request.POST.get('street', '')
+        house_number  = request.POST.get('house_number', '')
+        postcode      = request.POST.get('postcode', '')
+        city          = request.POST.get('city', '')
+        customer      = request.POST.get('customer', '')
+        status        = LocationController.save(
+            id            = id,
+            name          = name,
+            email_address = email_address,
+            phone_number  = phone_number,
+            street        = street,
+            house_number  = house_number,
+            postcode      = postcode,
+            city          = city,
+            customer      = customer,
+        )
+        response = JsonResponse(status.__dict__)
+        if status.status:
+            response.set_cookie('customer_status_status' , status.status , 7)
+            response.set_cookie('customer_status_message', status.message, 7)
+
+    return response
+
+def deleteLocation(request: WSGIRequest) -> HttpResponse:
+    response = JsonResponse({})
+    if request.is_ajax:
+        id       = request.POST.get('id', '')
+        status   = LocationController.delete(id = id)
+        response = JsonResponse(status.__dict__)
         response.set_cookie('customer_status_status' , status.status , 7)
         response.set_cookie('customer_status_message', status.message, 7)
 
