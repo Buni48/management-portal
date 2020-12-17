f5d095aaacf7cc7017a45dc0f3dc033f3257a66c
Buni48 <dago24@posteo.de>
2020-12-17 19:21:22 +0100

    Location create, edit & delete: urls

--- customers/urls.py
+++ customers/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.customerList, name='customers_list'),
-    path('customer/<int:id>/', views.customer, name='customer'),
+    path('<int:id>/', views.customer, name='customer'),
+    path('<int:customer_id>/locations/create/', views.createLocation, name='locations_create'),
+    path('<int:customer_id>/locations/edit/<int:id>/', views.editLocation, name='locations_edit'),
+    path('save-location/', views.saveLocation, name='locations_save'),
+    path('delete-location/', views.deleteLocation, name='locations_delete'),
     path('create/', views.create, name='customers_create'),
     path('edit/<int:id>/', views.edit, name = 'customers_edit'),
     path('save/', views.save, name='customers_save'),
