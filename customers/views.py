b2e93f3e816009cf90db807b03a5ada6fbcc194f
Buni48 <dago24@posteo.de>
2020-12-18 11:15:18 +0100

    Documentation of all views

--- customers/views.py
+++ customers/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
 from heartbeat.controllers import HeartbeatController
 from customers.controllers import CustomerController
 from .controllers import LocationController
 
 
-def index(request: WSGIRequest) -> HttpResponse:
+def index(request: WSGIRequest) -> HttpResponseRedirect:
+    """
+    When the app root is called. Redirects to the customer list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponseRedirect: redirection to the customer list
+    """
     return redirect('customers_list')
 
 def customerList(request: WSGIRequest) -> HttpResponse:
+    """
+    When the customer list is called. Renders the customer list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponse: customer list
+    """
     status       = request.COOKIES.get('customer_status_status')
     message      = request.COOKIES.get('customer_status_message')
     heartbeats   = HeartbeatController.read()
@@ -30,6 +48,16 @@ def customerList(request: WSGIRequest) -> HttpResponse:
     return response
 
 def customer(request: WSGIRequest, id: int = 0):
+    """
+    When a customer is called. Renders the customer page with the given id.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+    id      (int)        : id of the customer to load
+
+    Returns:
+    customer page
+    """
     response = None
     if request.is_ajax and id == 0:
         customer_number = request.POST.get('customer_number', '')
@@ -60,6 +88,15 @@ def customer(request: WSGIRequest, id: int = 0):
     return response
 
 def create(request: WSGIRequest) -> HttpResponse:
+    """
+    When the customer create is called. Renders the form to create a customer.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponse: form to create customer
+    """
     heartbeats = HeartbeatController.read()
     context = {
         'title'     : 'Kunden erstellen',
@@ -68,6 +105,17 @@ def create(request: WSGIRequest) -> HttpResponse:
     return render(request, 'customers/edit.html', context)
 
 def edit(request: WSGIRequest, id: int = 0) -> HttpResponse:
+    """
+    When the customer edit is called.
+    Renders the form to edit the customer with the given id. 
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+    id      (int)        : id of the customer to edit
+
+    Returns:
+    HttpResponse: form to edit customer
+    """
     if id < 1:
         return redirect('customers_list')
 
@@ -81,6 +129,17 @@ def edit(request: WSGIRequest, id: int = 0) -> HttpResponse:
     return render(request, 'customers/edit.html', context)
 
 def save(request: WSGIRequest) -> JsonResponse:
+    """
+    When the customer save is called as an ajax request.
+    Saves the data sent if valid and complete and returns the status.
+    If id is also sent this customer will be edited, otherwise a new one will be created.
+
+    Attributes:
+    request (WSGIRequest): ajax save request
+
+    Returns:
+    JsonResponse: save status
+    """
     response = JsonResponse({})
     if request.is_ajax:
         id              = request.POST.get('id', '')
@@ -98,7 +157,17 @@ def save(request: WSGIRequest) -> JsonResponse:
 
     return response
 
-def delete(request: WSGIRequest, id: int = 0) -> HttpResponse:
+def delete(request: WSGIRequest, id: int = 0) -> HttpResponseRedirect:
+    """
+    When the customer delete is called. Deletes the customer with the given id.
+    After that it redirects to the customer list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponseRedirect: redirection to the customer list
+    """
     response = redirect('customers_list')
     if id > 0:
         status = CustomerController.delete(id = id)
@@ -108,6 +177,17 @@ def delete(request: WSGIRequest, id: int = 0) -> HttpResponse:
     return response
 
 def createLocation(request: WSGIRequest, customer_id: int = 0) -> HttpResponse:
+    """
+    When the location create is called. Renders the form to create a location.
+    Pass the id of the customer the location should belong to.
+
+    Attributes:
+    request     (WSGIRequest): url request of the user
+    customer_id (int)        : id of the customer
+
+    Returns:
+    HttpResponse: form to create location
+    """
     if customer_id < 1:
         return redirect('customers_list')
 
@@ -121,6 +201,18 @@ def createLocation(request: WSGIRequest, customer_id: int = 0) -> HttpResponse:
     return render(request, 'customers/edit-location.html', context)
 
 def editLocation(request: WSGIRequest, id: int = 0, customer_id: int = 0) -> HttpResponse:
+    """
+    When the location edit is called. Renders the form to edit the location with the given id.
+    Pass also the id of the customer the location belongs to.
+
+    Attributes:
+    request     (WSGIRequest): url request of the user
+    customer_id (int)        : id of the customer
+    id          (int)        : id of the location to edit
+
+    Returns:
+    HttpResponse: form to edit location
+    """
     if id < 1 or customer_id < 1:
         return redirect('customers_list')
 
@@ -136,6 +228,17 @@ def editLocation(request: WSGIRequest, id: int = 0, customer_id: int = 0) -> Htt
     return render(request, 'customers/edit-location.html', context)
 
 def saveLocation(request: WSGIRequest) -> JsonResponse:
+    """
+    When the location save is called as an ajax request.
+    Saves the data sent if valid and complete and returns the status.
+    If id is also sent this location will be edited, otherwise a new one will be created.
+
+    Attributes:
+    request (WSGIRequest): ajax save request
+
+    Returns:
+    JsonResponse: save status
+    """
     response = JsonResponse({})
     if request.is_ajax:
         id            = request.POST.get('id', '')
@@ -165,7 +268,17 @@ def saveLocation(request: WSGIRequest) -> JsonResponse:
 
     return response
 
-def deleteLocation(request: WSGIRequest) -> HttpResponse:
+def deleteLocation(request: WSGIRequest) -> JsonResponse:
+    """
+    When the location delete is called as an ajax request.
+    Deletes the location with the given id and returns the status.
+
+    Attributes:
+    request (WSGIRequest): ajax delete request
+
+    Returns:
+    JsonResponse: delete status
+    """
     response = JsonResponse({})
     if request.is_ajax:
         id       = request.POST.get('id', '')
--- licenses/views.py
+++ licenses/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from heartbeat.controllers import HeartbeatController
 from .controllers import LicenseController, SoftwareModuleController
 from customers.controllers import CustomerController, LocationController
 
-def index(request: WSGIRequest) -> HttpResponse:
+def index(request: WSGIRequest) -> HttpResponseRedirect:
+    """
+    When the app root is called. Redirects to the license list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponseRedirect: redirection to the license list
+    """
     return redirect('licenses_list')
 
 def licensesList(request: WSGIRequest) -> HttpResponse:
+    """
+    When the license list is called. Renders the license list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponse: license list
+    """
     status     = request.COOKIES.get('license_status_status')
     message    = request.COOKIES.get('license_status_message')
     heartbeats = HeartbeatController.read()
@@ -27,6 +45,15 @@ def licensesList(request: WSGIRequest) -> HttpResponse:
     return response
 
 def create(request: WSGIRequest) -> HttpResponse:
+    """
+    When the license create is called. Renders the form to create a license.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponse: form to create license
+    """
     heartbeats = HeartbeatController.read()
     modules    = SoftwareModuleController.getModuleNames()
     locations  = LocationController.getLocationNames()
@@ -41,6 +68,17 @@ def create(request: WSGIRequest) -> HttpResponse:
     return render(request, 'licenses/edit.html', context)
 
 def edit(request: WSGIRequest, id: int = 0) -> HttpResponse:
+    """
+    When the license edit is called.
+    Renders the form to edit the license with the given id. 
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+    id      (int)        : id of the license to edit
+
+    Returns:
+    HttpResponse: form to edit license
+    """
     if id < 1:
         return redirect('licenses_list')
 
@@ -60,6 +98,17 @@ def edit(request: WSGIRequest, id: int = 0) -> HttpResponse:
     return render(request, 'licenses/edit.html', context)
 
 def save(request: WSGIRequest) -> JsonResponse:
+    """
+    When the license save is called as an ajax request.
+    Saves the data sent if valid and complete and returns the status.
+    If id is also sent this license will be edited, otherwise a new one will be created.
+
+    Attributes:
+    request (WSGIRequest): ajax save request
+
+    Returns:
+    JsonResponse: save status
+    """
     response = JsonResponse({})
     if request.is_ajax():
         id          = request.POST.get('id', '')
@@ -88,7 +137,17 @@ def save(request: WSGIRequest) -> JsonResponse:
 
     return response
 
-def delete(request: WSGIRequest, id: int = 0) -> HttpResponse:
+def delete(request: WSGIRequest, id: int = 0) -> HttpResponseRedirect:
+    """
+    When the license delete is called. Deletes the license with the given id.
+    After that it redirects to the license list.
+
+    Attributes:
+    request (WSGIRequest): url request of the user
+
+    Returns:
+    HttpResponseRedirect: redirection to the license list
+    """
     response = redirect('licenses_list')
     if id > 0:
         status = LicenseController.delete(id = id)
